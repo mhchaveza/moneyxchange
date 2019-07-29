ba19b225c47d4ad2070ff6735bc5639e502afebf
Milton Horacio Chàvez <milton-ch@hotmail.com>
2019-07-28 19:51:28 -0500

    base app

---
+++ src/app/models/currrency-register.spec.ts
@@ -0,0 +1,7 @@
+import { CurrrencyRegister } from './currrency-register';
+
+describe('CurrrencyRegister', () => {
+  it('should create an instance', () => {
+    expect(new CurrrencyRegister()).toBeTruthy();
+  });
+});
